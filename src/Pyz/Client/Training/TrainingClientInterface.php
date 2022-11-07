325f426e0b27e7e0fdf34d3ffe4a318fb9d06bbc
Hidran <hidran.arias@spryker.com>
2022-11-07 22:04:39 +0100

    Extending Spryker functionality - Part 2. Adding createantelope to the client

--- src/Pyz/Client/Training/TrainingClientInterface.php
+++ src/Pyz/Client/Training/TrainingClientInterface.php
@@ -4,8 +4,11 @@ namespace Pyz\Client\Training;
 
 use Generated\Shared\Transfer\AntelopeCriteriaTransfer;
 use Generated\Shared\Transfer\AntelopeResponseTransfer;
+use Generated\Shared\Transfer\AntelopeTransfer;
 
 interface TrainingClientInterface
 {
     public function findAntelope(AntelopeCriteriaTransfer $antelopeCriteria): AntelopeResponseTransfer;
+
+    public function createAntelope(AntelopeTransfer $antelopeTransfer): AntelopeTransfer;
 }
